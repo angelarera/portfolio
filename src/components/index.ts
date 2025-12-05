7d3591b409b73f843dcdb22fec9f56b5b8115d0a
Ángela Real <angelarealr@gmail.com>
2025-12-05 13:13:16 +0100

    Typography variables declared

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from "./Header.astro";
 export { default as Footer } from "./Footer.astro";
 export { default as ThemeSwitcher } from "./ThemeSwitcher.astro";
+export { default as Arrow } from "./Arrow.astro";
